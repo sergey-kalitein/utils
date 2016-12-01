3b2c3decf25e264ee220d0ff61ab94dad1bde2a2
sergey-kalitein <sergey.kalitein@autoxloo.com>
2016-12-01 16:55:14 +0200

    Cleaning up

--- ax11tocsv.php
+++ ax11tocsv.php
@@ -4,7 +4,6 @@
 ini_set('memory_limit', '2G');
 
 $nameInputFile = __DIR__ . "/WRCS.xml";
-//$nameInputFile = 'http://www.dfi.globalcarexchange.com/WRCS.xml';
 echo 'Loading the input source data from: ' . $nameInputFile . ' ...';
 $xml = simplexml_load_file($nameInputFile, 'SimpleXMLElement', LIBXML_PARSEHUGE | LIBXML_NOBLANKS);
 $json = json_decode(json_encode($xml), JSON_OBJECT_AS_ARRAY);
